921deb4867e677f918db747f436782ff35db652f
KamilPurcelSumm-It <162700572+KamilPurcelSumm-It@users.noreply.github.com>
2026-07-08 16:02:20 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "standalone", // <--- Dodana linijka wymuszająca prawidłowe budowanie dla Azure
+};
 
 export default withNextIntl(nextConfig);
