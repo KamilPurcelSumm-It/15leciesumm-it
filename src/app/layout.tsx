53715f49c57f75d66299abbe39874500537dbeb4
warsztaty06 <warsztaty06@summitdevtestcompany.onmicrosoft.com>
2026-07-21 12:44:59 +0200

    Ikona summit2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "15-lecie summ-it",
+  icons: {
+    icon: [{ url: "/logo_summit.png", type: "image/png" }],
+    shortcut: "/logo_summit.png",
+  },
+};
 
 export default function RootLayout({
   children,
